0581451b0a12acdde3b39d27dd6dc913f6a80a61
Alasha <alasha.khadzar@gmail.com>
2021-11-11 14:47:49 +0300

    - enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   app.setGlobalPrefix('api');
 
   app.use(cookieParser());
