3cb9fcb64ee30f4dc07a8fb26b2c2a278a522617
blessmepadre <alan.hadzaragov@saltpepper.ru>
2022-01-13 16:16:09 +0300

    - returned the previous cookies behaviour

--- src/templates/cookies.ts
+++ src/templates/cookies.ts
@@ -1,8 +1,7 @@
 import { template } from 'lodash';
 
 export const signInCookie = template(
-  'Authentication=<%= token %>; HttpOnly; SameSite=None; Path=/; Max-Age=<%= exp %>',
+  'Authentication=<%= token %>; HttpOnly; Path=/; Max-Age=<%= exp %>',
 );
 
-export const signOutCookie =
-  'Authentication=; HttpOnly; SameSite=None; Path=/; Max-Age=0';
+export const signOutCookie = 'Authentication=; HttpOnly; Path=/; Max-Age=0';
